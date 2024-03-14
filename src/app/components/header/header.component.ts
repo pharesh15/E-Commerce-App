ec6f2400021aab3559912b912c1e91a955a5c47e
haresh <hp452444@gmail.com>
2024-03-14 23:35:27 +0530

    header changed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,23 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  menuType: string = "DEFAULT";
   constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.router.events.subscribe((route: any) => {
-      if (route) {
+      if (route.url) {
         if (localStorage.getItem("sellerData") && route.url.includes("seller")) {
-          console.log("Inside seller");
+          this.menuType = "SELLER";
         } else {
-          console.log("outside");
+          this.menuType = "DEFAULT";
         }
       }
     })
